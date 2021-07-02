f823562e96cf476c1b01b9634c0b04fbd7ad45be
Toshi <tosmow@gmail.com>
2021-07-02 12:16:35 -0600

    Hi

--- persist.js
+++ persist.js
@@ -2,7 +2,17 @@ const mongoose = require("mongoose")
 
 
 function connect(callback) {
-    callback();
+    let connectionString = `mongodb+srv://tilder_2021:myCoolPassword@cluster0.jpjzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    console.log("Connecting to server...")
+
+    mongoose.connect(
+        connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).catch((err) =>{
+        console.log("There was an error connecting to mango: ", err);
+    })
+    mongoose.connection.once('open', callback)
 }
 
 module.exports = connect;
--- server.js
+++ server.js
@@ -5,3 +5,15 @@ const server = express()
 server.use(cors())
 server.use(express.json({}))
 module.exports = server;
+
+//GET /thread
+
+//GET /thread/:id
+
+//POST /thread
+
+//DELETE /thread:id
+
+// POST /post
+
+//DELETE /post/:thread_id/:post_id
