97aaafba618acdcf5bd57486b8e65f0fa410f9e3
Toshi <tosmow@gmail.com>
2021-07-06 09:47:58 -0600

    sending a message

--- model.js
+++ model.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const threadSchema = mongoose.Schema({
     name: String,
     description: String,
-    done: Boolean,
-    deadline: Date
+    author: String,
+    category: String,
+    createdAt: Date,
+    updatedAt: Date,
 });
-
+const postSchema = mongoose.Schema({
+    name:String
+})
 const Thread = mongoose.model("Thread", threadSchema)
 
 let store = {};
--- server.js
+++ server.js
@@ -51,8 +51,20 @@ server.post("/thread", (req,res)=>{
 //DELETE /thread:id
 server.delete("/thread/:id", (req,res)=>{
     res.setHeader("Content-Type", "application/json");
-    console.log()
+    console.log(`deleting a thread with id ${req.paramas.id}`);
+    res.json({});
 })
 //POST /post
+server.post("/post", (req, res)=>{
+    res.setHeader("Content-Type", "application/json");
+    console.log(`Creating a postwith body ${req.body}`);
+    res.json({});
 
+})
 //DELETE /post/:thread_id/:post_id
+server.post("/post/:thread_id/:post_id", (req, res)=>{
+    res.setHeader("Content-Type", "application/json");
+    console.log(`My deleted post's thread's id is ${req.params.thread_id} 
+    and the post's id is ${res.params.post_id}`);
+    res.json({});
+})
