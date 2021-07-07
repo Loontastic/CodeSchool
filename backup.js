4827a2dd2674ac5ae44908fd3618aecb3ab4822b
Toshi <tosmow@gmail.com>
2021-07-07 12:34:26 -0600

    test

--- backup.js
+++ backup.js
@@ -1,5 +1,22 @@
-//pull all threads
 var fs = require("fs");
 const {Post, Thread} = require('./model')
-
+//pull all threads
+masterList = []
+function toGrabThread(){
+    //code
+    //masterList.push(thread)
+}
+function toGrabPosts(){
+    //code
+    //masterList.push(post)
+}
+function deleteDatabase(){
+    //code
+}
+function createThreads(){
+    //code
+}
+function createPosts(){
+    //code
+}
 //pull posts from specific thread
