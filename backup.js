e86365171440a62660f437f2d3af89d35258fc54
Aris <nasiraselano@gmail.com>
2021-07-07 12:40:08 -0600

    toGrabThread and toGrabPost functions written

--- backup.js
+++ backup.js
@@ -4,10 +4,44 @@ const {Post, Thread} = require('./model')
 masterList = []
 function toGrabThread(){
     //code
+    console.log("Getting the Threads");
+    Thread.find({}, (err, thread)=>{
+        if (err){
+            console.log("There was an error getting the Threads")
+            res.status(500).send(
+                JSON.stringify({message:"Unable to grab the threads",
+                error:err})
+            );
+            return;
+        }
+        // res.status(200).json(thread);
+        masterList.push(thread);
+        req.local = {
+            error: "this is my error message"
+        }
+        next();
+    });
     //masterList.push(thread)
 }
 function toGrabPosts(){
     //code
+    console.log("Getting the Posts");
+    Post.find({}, (err, post)=>{
+        if (err){
+            console.log("There was an error getting the Posts")
+            res.status(500).send(
+                JSON.stringify({message:"Unable to grab the posts",
+                error:err})
+            );
+            return;
+        }
+        // res.status(200).json(thread);
+        masterList.push(post);
+        req.local = {
+            error: "this is my error message"
+        }
+        next();
+    });
     //masterList.push(post)
 }
 function deleteDatabase(){
