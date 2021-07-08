f1c6b2325b882cc8e696bb3fdb458a09cf88a4ba
Aris <nasiraselano@gmail.com>
2021-07-08 10:09:39 -0600

    commented again

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const server = require("./server");
 const persist = require("./persist");
 
-const port = process.argv[2]||process.env.PORT ||8080;
+// define a port
+const port = process.argv[2] || process.env.PORT || 8080;
 
 persist(()=>{
     server.listen(port, ()=>{
+        // console log for server startup
         console.log(`Code School 2021 Forum App Running on port ${port}`);
     })
 })
